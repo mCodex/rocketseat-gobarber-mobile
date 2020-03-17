6fa81ce532a1b35faebffe4ea7b188bff35dcb1c
Mateus Andrade <mat.andrade@live.com>
2020-03-17 19:52:25 -0300

    added initial routes & configs

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { enableScreens } from 'react-native-screens';
+import App from './src/routes';
+import { name as appName } from './app.json';
+
+import 'react-native-gesture-handler';
+
+enableScreens();
 
 AppRegistry.registerComponent(appName, () => App);
