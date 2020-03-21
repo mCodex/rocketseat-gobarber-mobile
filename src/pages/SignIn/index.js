eb5193e2893d43a67aac32a24efbf553d08332f2
Mateus Andrade <mat.andrade@live.com>
2020-03-21 10:40:20 -0300

    added Button & Input component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 export default () => {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input icon="call" />
+      <Button>Entrar</Button>
     </Background>
   );
 };
