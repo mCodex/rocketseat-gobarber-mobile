600a7497b5d10db58343e8a850d147a10ecc9f34
Mateus Andrade <mat.andrade@live.com>
2020-03-27 19:34:06 -0300

    added date picker input & selecting date from provider's list

---
+++ src/components/DateInput/index.android.js
@@ -0,0 +1,37 @@
+import React, { useState, useMemo } from 'react';
+import { DatePickerAndroid } from 'react-native';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, DateButton, DateText, Picker } from './styles';
+
+export default ({ date, onChange }) => {
+  const [opened, setOpened] = useState(false);
+
+  const dateFormatted = useMemo(() => {
+    return format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt });
+  }, [date]);
+
+  const handleOpenPicker = async () => {
+    const { action, year, month, day } = await DatePickerAndroid.open({
+      mode: 'spinner',
+      date,
+    });
+
+    if (action === DatePickerAndroid.dateSetAction) {
+      const selectedDate = new Date(year, month, day);
+      onChange(selectedDate);
+    }
+  };
+
+  return (
+    <Container>
+      <DateButton onPress={() => setOpened(!opened)}>
+        <Icon name="event" color="#fff" size={20} />
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+    </Container>
+  );
+};
--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
 
-export default () => {
-  return <Background />;
+import api from '~/services/api';
+
+import { Container, HourList, Hour, Title } from './styles';
+
+export default ({ navigation, route }) => {
+  const { provider } = route.params;
+  const [hours, setHours] = useState([]);
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const loadAvailable = async () => {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    };
+    loadAvailable();
+  }, [date, provider.id]);
+
+  const handleSelectHour = time => {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  };
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          extraData={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              enabled={item.available}
+              onPress={() => handleSelectHour(item.value)}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 };
